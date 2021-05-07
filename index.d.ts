a2d27972c8f0b1d04f50eed3c780410d6fab2d1a
farhad <mehryari.f@gmail.com>
2021-05-07 20:05:45 +0430

    Fix export

--- index.d.ts
+++ index.d.ts
@@ -1,33 +1,15 @@
 import { Application, Router } from "express"
-import { NuxtService as NuxtServiceClass } from "./src"
+import {App, NuxtService as NuxtServiceClass } from "./src"
 
-type App = {
-    router: Router
-    server: Application
 
-    constructor(port: number)
-
-    registerService(service: NuxtServiceClass): void
-}
 
 type NuxtService = {
     constructor(config: ServiceConfig)
 }
 
 interface GeshnizRouter {
-    App: {
-        router: Router
-        server: Application
-
-        constructor(port: number)
-
-        registerService(service: NuxtServiceClass): void
-    }
-    NuxtService: {
-        constructor(config: ServiceConfig)
-    }
+    App
+    NuxtService
 }
 
-declare const geshnizRouter: GeshnizRouter
-
-export default geshnizRouter
+export default App
