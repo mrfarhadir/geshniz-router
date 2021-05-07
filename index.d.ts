7d0cdfdfc79c7e2a937fea4896421cd15db8f625
farhad <mehryari.f@gmail.com>
2021-05-07 17:09:58 +0430

    Add required types to index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,22 @@
+import { Application, Router } from "express"
+import { NuxtService as NuxtServiceClass } from "./src"
+
+interface App {
+    router: Router
+    server: Application
+
+    constructor(port: number)
+
+    registerService(service: NuxtServiceClass): void
+}
+
+interface NuxtService {
+    constructor(config: ServiceConfig)
+}
+
 type GeshnizRouter = {
-    App: object,
-    NuxtService: object
+    App
+    NuxtService
 }
 
 declare const geshnizRouter: GeshnizRouter
